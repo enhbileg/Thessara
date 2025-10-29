99dca919c044c3d9e5bf06eab0b9cc9a9dc5a618
Enkhbileg <enh.bom8@gmail.com>
2025-10-30 07:45:18 +0800

    pisda2

--- config/db.js
+++ config/db.js
@@ -16,7 +16,7 @@ async function connectDB() {
     const opts = {
       bufferCommands: false,
     }
-        cached.promise = mongoose.connect(process.env.MONGODB_URI/Thessara, opts).then(mongoose => {
+        cached.promise = mongoose.connect(process.env.MONGODB_URI/'Thessara', opts).then(mongoose => {
         return mongoose
       
     })
