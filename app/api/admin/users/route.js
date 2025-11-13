e6aa4195eef36a3c8bf8869917d56096159a0aa0
Enkhbileg <enh.bom8@gmail.com>
2025-11-13 09:55:36 +0800

    0.8.9.1

--- app/api/admin/users/route.js
+++ app/api/admin/users/route.js
@@ -1,8 +1,16 @@
 import connectDB from "@/config/db";
 import User from "@/models/User";
+import { NextResponse } from "next/server";
 
 export async function GET() {
-  await connectDB();
-  const users = await User.find({});
-  return Response.json(users);
+  try {
+    await connectDB();
+    const users = await User.find().select("name email imageUrl cartItems");
+    return NextResponse.json({ success: true, users }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: error.message },
+      { status: 500 }
+    );
+  }
 }
